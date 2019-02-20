a6a273dd7f362eaad3ae95d7f8ea0fa9c6197628
Postnov Daniil <dan.postnov@gmail.com>
2019-02-20 07:42:34 +0300

    add markup and styles in Add component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,25 @@
+<template>
+    <form class="add-todo d-flex">
+        <input type="text" class="form-control" placeholder="Enter you task in here" value="">
+        <button class="btn btn-outline-secondary">Add item</button>
+    </form>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .add-todo {
+        margin-top: 20px
+    }
+
+    .add-todo input {
+        margin-right: 3px;
+        -webkit-flex: 1 0;
+        flex: 1 0
+    }
+</style>
+
