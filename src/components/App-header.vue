957b4e619946121a29dbec0bab254717aa5abe38
Postnov Daniil <dan.postnov@gmail.com>
2019-02-20 07:43:42 +0300

    add markup and styles in Appheader component

---
+++ src/components/App-header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="app-header d-flex">
+        <h1>My todo list</h1>
+        <h2>3 more to do, 0 done</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+    .app-header {
+        -webkit-align-items: flex-end;
+        align-items: flex-end
+    }
+
+    .app-header h1 {
+        -webkit-flex-grow: 1;
+        flex-grow: 1
+    }
+
+    .app-header h2 {
+        font-size: 1.2rem;
+        color: grey
+    }
+</style>
+
