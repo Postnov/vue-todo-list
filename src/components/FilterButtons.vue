fa1216d5cbb64eff95d9485cc880e25b61e97c2b
Postnov Daniil <dan.postnov@gmail.com>
2019-02-20 07:46:14 +0300

    rename and markup in filterButtons component

---
+++ src/components/FilterButtons.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="btn-group">
+        <button type="button" class="btn btn-info">All</button>
+        <button type="button" class="btn btn-outline-secondary">Active</button>
+        <button type="button" class="btn btn-outline-secondary">Done</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
