d7493f3d1f432f5d591d2cc9dab476f55755a38d
Postnov Daniil <dan.postnov@gmail.com>
2019-02-20 07:49:21 +0300

    add  searchPanel markup, styles

---
+++ src/components/Search-panel.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="text" class="form-control search-panel" placeholder="Search">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.search-panel {
+    width: auto;
+    -webkit-flex-grow: 1;
+    flex-grow: 1;
+    margin-right: 3px
+}
+</style>
+
+
