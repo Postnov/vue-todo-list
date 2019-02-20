933d792f0687f6241074e121974638bc2ce00e3d
Postnov Daniil <dan.postnov@gmail.com>
2019-02-20 07:50:05 +0300

    add App component

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="todo-app">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.todo-app {
+    margin: 2rem auto 0;
+    max-width: 400px
+}
+
+.top-panel {
+    margin: 1rem 0
+}
+</style>
+
